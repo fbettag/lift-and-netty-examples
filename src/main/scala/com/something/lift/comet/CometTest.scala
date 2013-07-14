bf741a1af2fbe6a49e5416d4a9b24a6b4565e1bd
Franz Bettag <franz@bett.ag>
2013-07-14 21:19:56 +0200

    Added Comet component for testing. (fails)

---
+++ src/main/scala/com/something/lift/comet/CometTest.scala
@@ -0,0 +1,21 @@
+package com.something.lift.comet
+
+import net.liftweb.actor._
+import net.liftweb.common._
+import net.liftweb.util._
+import net.liftweb.http._
+import scala.xml._
+import net.liftweb.http.js.JsCmds.SetHtml
+
+class CometTest extends CometActor {
+
+  override def defaultPrefix = Full("comet-test-prefix")
+
+  override def messageHandler = {
+    case a: String => partialUpdate(SetHtml("comet-test-output", Text(a)))
+  }
+
+  def render = {
+    "#comet-test-input" #> SHtml.ajaxText("Input here", (x) => this ! x)
+  }
+}
